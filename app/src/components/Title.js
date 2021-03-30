29c03d820a0172ffe319c66286d6460a1d57edcf
g1eb <mail@g1eb.com>
2021-03-30 15:12:20 -0700

    Add a separate component for the app title

---
+++ app/src/components/Title.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/styles'
+
+import Logo from './Logo'
+
+
+const useStyles = makeStyles(theme => ({
+  header: {
+    color: '#fefefe',
+    marginTop: theme.spacing(3),
+  },
+  logo: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    width: theme.spacing(12),
+    height: theme.spacing(12),
+    marginRight: theme.spacing(2),
+  },
+}))
+
+
+const Title = ({ title }) => {
+
+  const classes = useStyles()
+
+  return (
+    <Typography
+      component="h3"
+      variant="h3"
+      className={ classes.header }>
+      <a href="https://github.com/usc-isi-i2/kgtk" title="Knowledge Graph Toolkit" rel="noopener noreferrer nofollow" target="_blank">
+        <div className={ classes.logo }>
+          <Logo/>
+        </div>
+      </a>
+      {title}
+    </Typography>
+  )
+}
+
+
+export default Title
