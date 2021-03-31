23d09366355f613e2df4d01ffcf3b6f230d26d8d
g1eb <mail@g1eb.com>
2021-03-30 19:37:42 -0700

    Move the input field overrides to the global app component

--- app/src/App.js
+++ app/src/App.js
@@ -27,7 +27,58 @@ const styles = theme => ({
 
 const App = () => {
 
-  const theme = createMuiTheme()
+  const theme = createMuiTheme({
+    overrides: {
+      MuiTextField: {
+        root: {
+          color: '#fefefe',
+          '& .MuiFormLabel-root': {
+            '@media (min-width:600px)': {
+              fontSize: '1.25rem',
+              opacity: 0.85,
+            },
+            color: '#fefefe',
+          },
+          '&.small .MuiFormLabel-root': {
+            '@media (min-width:600px)': {
+              fontSize: '1rem',
+            }
+          },
+          '& .MuiInput-input': {
+            '@media (min-width:600px)': {
+              fontSize: '2rem',
+            },
+            color: '#fefefe',
+            transition: 'background 0.3s ease',
+          },
+          '&.small .MuiInput-input': {
+            '@media (min-width:600px)': {
+              fontSize: '1.5rem'
+            }
+          },
+          '& label.Mui-focused': {
+            color: '#fefefe',
+          },
+          '&:hover .MuiInput-input': {
+            background: 'rgba(255, 255, 255, 0.1)',
+          },
+          '&:hover .MuiInput-underline:before': {
+            borderBottomColor: '#fefefe',
+            borderBottom: '3px solid',
+          },
+          '& .MuiInput-underline:before': {
+            borderBottomColor: '#fefefe',
+          },
+          '& .MuiInput-underline:after': {
+            borderBottomColor: '#fefefe',
+          },
+          '& .MuiInputLabel-shrink': {
+            transform: 'translate(0px, -10px)',
+          },
+        },
+      },
+    },
+  })
 
   return (
     <ThemeProvider theme={theme}>
--- app/src/components/Input.js
+++ app/src/components/Input.js
@@ -4,55 +4,6 @@ import TextField from '@material-ui/core/TextField'
 import { withStyles } from '@material-ui/core/styles'
 
 
-const CustomTextField = withStyles({
-  root: {
-    '& .MuiFormLabel-root': {
-      '@media (min-width:600px)': {
-        fontSize: '1.25rem',
-        opacity: 0.85,
-      },
-      color: '#fefefe',
-    },
-    '&.small .MuiFormLabel-root': {
-      '@media (min-width:600px)': {
-        fontSize: '1rem',
-      }
-    },
-    '& .MuiInput-input': {
-      '@media (min-width:600px)': {
-        fontSize: '2rem',
-      },
-      color: '#fefefe',
-      transition: 'background 0.3s ease',
-    },
-    '&.small .MuiInput-input': {
-      '@media (min-width:600px)': {
-        fontSize: '1.5rem'
-      }
-    },
-    '& label.Mui-focused': {
-      color: '#fefefe',
-    },
-    '&:hover .MuiInput-input': {
-      background: 'rgba(255, 255, 255, 0.1)',
-    },
-    '&:hover .MuiInput-underline:before': {
-      borderBottomColor: '#fefefe',
-      borderBottom: '3px solid',
-    },
-    '& .MuiInput-underline:before': {
-      borderBottomColor: '#fefefe',
-    },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: '#fefefe',
-    },
-    '& .MuiInputLabel-shrink': {
-      transform: 'translate(0px, -10px)',
-    },
-  },
-})(TextField)
-
-
 const Input = ({ autoFocus, label, onChange }) => {
 
   const inputElement = useRef(null)
@@ -62,7 +13,7 @@ const Input = ({ autoFocus, label, onChange }) => {
   }
 
   return (
-    <CustomTextField
+    <TextField
       ref={element => inputElement.current = element}
       id={'q'}
       name={'q'}
