594eb3049eb41e4829b516f90e2c230151c0aeaa
g1eb <mail@g1eb.com>
2021-03-30 17:44:48 -0700

    Add the search to the test nodes component

--- app/src/components/TestNodes.js
+++ app/src/components/TestNodes.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/styles'
+
+
+import Input from './Input'
 
 
 const useStyles = makeStyles(theme => ({
@@ -41,6 +44,9 @@ const TestNodes = ({ subject }) => {
 
   const classes = useStyles()
 
+  const timeoutID = useRef(null)
+
+  const [query, setQuery] = useState('')
   const [results, setResults] = useState([])
 
   const renderHeader = () => {
@@ -80,10 +86,75 @@ const TestNodes = ({ subject }) => {
     return results.map(result => {JSON.stringify(result)})
   }
 
+  const submitQuery = query => {
+    // Construct the url with correct parameters
+    let url = `https://kgtk.isi.edu/api/`
+    if ( query ) {
+      url += `${query}?type=ngram&extra_info=true&language=en&item=qnode`
+      return fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => response.json())
+      .then((results) => {
+        setResults(results)
+      })
+    }
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    submitQuery(query)
+  }
+
+  const handleOnChange = query => {
+    setQuery(query)
+    if ( !query ) {
+      setResults([])
+    } else {
+      clearTimeout(timeoutID.current)
+      timeoutID.current = setTimeout(() => {
+        submitQuery(query)
+      }, 500)
+    }
+  }
+
+  const renderSearch = () => {
+    return (
+      <Grid container spacing={3} className={classes.wrapper}>
+        <Grid item xs={3}>
+        </Grid>
+        <Grid item xs={8}>
+          <form className={ classes.form } noValidate
+            onSubmit={event => handleSubmit(event)}>
+            <Grid container spacing={ 3 }>
+              <Grid item xs={ 12 }>
+                <Paper component="div" className={ classes.paper } square>
+                  <Grid container spacing={ 3 }>
+                    <Grid item xs={ 12 }>
+                      <Input autoFocus={ true } label={'search to compare q-nodes'}
+                        onChange={query => handleOnChange(query)}/>
+                    </Grid>
+                  </Grid>
+                </Paper>
+                {renderResults()}
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+        <Grid item xs={1}>
+        </Grid>
+      </Grid>
+    )
+  }
+
   return (
     <React.Fragment>
       {renderHeader()}
       {renderResults()}
+      {renderSearch()}
     </React.Fragment>
   )
 }
