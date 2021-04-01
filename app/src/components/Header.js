91581f2b35d971a73257661436d3c64aed3cdeb4
g1eb <mail@g1eb.com>
2021-04-01 11:52:59 -0700

    Add download csv button to the header component

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import GetAppIcon from '@material-ui/icons/GetApp'
+import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/styles'
 
@@ -18,6 +20,21 @@ const useStyles = makeStyles(theme => ({
     height: theme.spacing(12),
     marginRight: theme.spacing(2),
   },
+  download: {
+    color: '#fefefe',
+    cursor: 'pointer',
+    position: 'absolute',
+    top: theme.spacing(6),
+    right: theme.spacing(4),
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+    '& svg.MuiSvgIcon-root': {
+      color: '#fefefe',
+      verticalAlign: 'bottom',
+      marginLeft: theme.spacing(2),
+    },
+  },
 }))
 
 
@@ -26,20 +43,29 @@ const Header = () => {
   const classes = useStyles()
 
   return (
-    <Typography
-      component="h3"
-      variant="h3"
-      className={ classes.header }>
-      <a href="https://github.com/usc-isi-i2/kgtk"
-        title="Knowledge Graph Toolkit"
-        rel="noopener noreferrer nofollow"
-        target="_blank">
-        <div className={ classes.logo }>
-          <Logo/>
-        </div>
-      </a>
-      Knowledge Graph Semantic Similarity
-    </Typography>
+    <React.Fragment>
+      <Typography
+        component="h3"
+        variant="h3"
+        className={ classes.header }>
+        <a href="https://github.com/usc-isi-i2/kgtk"
+          title="Knowledge Graph Toolkit"
+          rel="noopener noreferrer nofollow"
+          target="_blank">
+          <div className={ classes.logo }>
+            <Logo/>
+          </div>
+        </a>
+        Knowledge Graph Semantic Similarity
+      </Typography>
+      <Typography
+        component="h5"
+        variant="h5"
+        className={ classes.download }>
+        Download CSV
+        <GetAppIcon fontSize="large" />
+      </Typography>
+    </React.Fragment>
   )
 }
 
