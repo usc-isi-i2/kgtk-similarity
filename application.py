940a3c749ff9ff7fa774e5a29c442d52eaf29c4b
saggu <amandeep.s.saggu@gmail.com>
2021-04-02 10:07:24 -0700

    add nn resource

--- application.py
+++ application.py
@@ -3,13 +3,14 @@ from flask_cors import CORS
 from flask_restful import Api
 from app_config import host, port
 from semantic_similarity.main import QnodeSimilarity
+from semantic_similarity.main import NN
 
 app = Flask(__name__)
 CORS(app)
 
-
 api = Api(app)
 api.add_resource(QnodeSimilarity, '/similarity')
+api.add_resource(NN, '/nearest-neighbors')
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
