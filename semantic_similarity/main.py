89660e575877f2bbfa03d8c96d9c4b2717cbb511
saggu <amandeep.s.saggu@gmail.com>
2021-04-02 10:09:18 -0700

    add resource fot nearest neighbors

--- semantic_similarity/main.py
+++ semantic_similarity/main.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restful import Resource
 from semantic_similarity.semantic_similarity import SemanticSimilarity
+from k_nearest_neighbors import FAISS_Index
 
 
 class QnodeSimilarity(Resource):
@@ -18,3 +19,15 @@ class QnodeSimilarity(Resource):
             return {'error': "embedding_type should be one of ['complex', 'text', 'transe']"}
 
         return self.ss.semantic_similarity(q1, q2, embedding_type)
+
+
+class NN(Resource):
+    fi = FAISS_Index()
+
+    def get(self):
+        qnode = request.args.get("qnode")
+        if qnode is None:
+            return {'error': "The url parameter: 'qnode' required"}
+
+        k = int(request.args.get('k', 5))
+        return self.fi.get_neighbors(qnode, get_scores=True, k=k)
