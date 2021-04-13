a4c34b8b9425080c9bcb49678cf93632527389bf
g1eb <mail@g1eb.com>
2021-04-13 16:25:26 -0700

    Use material ui grid system to align text and buttons in the app header

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import GetAppIcon from '@material-ui/icons/GetApp'
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import GetAppIcon from '@material-ui/icons/GetApp'
 import { makeStyles } from '@material-ui/styles'
 
 import Logo from './Logo'
@@ -23,9 +24,7 @@ const useStyles = makeStyles(theme => ({
     color: '#fefefe',
     cursor: 'pointer',
     userSelect: 'none',
-    position: 'absolute',
-    top: theme.spacing(6),
-    right: theme.spacing(4),
+    marginTop: theme.spacing(5),
     '&:hover': {
       textDecoration: 'underline',
     },
@@ -43,21 +42,24 @@ const Header = ({ subject, selected, download }) => {
   const classes = useStyles()
 
   return (
-    <React.Fragment>
-      <Typography
-        component="h3"
-        variant="h3"
-        className={ classes.header }>
-        <a href="https://github.com/usc-isi-i2/kgtk"
-          title="Knowledge Graph Toolkit"
-          rel="noopener noreferrer nofollow"
-          target="_blank">
-          <div className={ classes.logo }>
-            <Logo/>
-          </div>
-        </a>
-        Knowledge Graph Semantic Similarity
-      </Typography>
+    <Grid container spacing={3} justify="space-between">
+      <Grid item>
+        <Typography
+          component="h3"
+          variant="h3"
+          className={ classes.header }>
+          <a href="https://github.com/usc-isi-i2/kgtk"
+            title="Knowledge Graph Toolkit"
+            rel="noopener noreferrer nofollow"
+            target="_blank">
+            <div className={ classes.logo }>
+              <Logo/>
+            </div>
+          </a>
+          Knowledge Graph Semantic Similarity
+        </Typography>
+      </Grid>
+      <Grid item>
       {!!subject && !!selected.length && (
         <Typography
           component="h5"
@@ -68,7 +70,8 @@ const Header = ({ subject, selected, download }) => {
           <GetAppIcon fontSize="large" />
         </Typography>
       )}
-    </React.Fragment>
+      </Grid>
+    </Grid>
   )
 }
 
