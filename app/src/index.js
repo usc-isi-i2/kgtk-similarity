cc08a8755f4b27b3481557de5bae1be6440195d9
g1eb <mail@g1eb.com>
2021-04-13 19:39:02 -0700

    Report web vitals of the app to google analytics to measure performance

--- app/src/index.js
+++ app/src/index.js
@@ -12,7 +12,20 @@ ReactDOM.render(
 )
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+const sendToGoogleAnalytics = ({name, delta, value, id}) => {
+  // Assumes the global `gtag()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/ga4
+  window.gtag('event', name, {
+    // Use the metric delta as the event's value parameter.
+    value: delta,
+    // Everything below is a custom event parameter.
+    web_vitals_metric_id: id, // Needed to aggregate events.
+    web_vitals_metric_value: value, // Optional
+    // Any additional params or metadata (e.g. debug information) can be
+    // set here as well, within the following limitations:
+    // https://support.google.com/analytics/answer/9267744
+  })
+}
+
+
+reportWebVitals(sendToGoogleAnalytics)
