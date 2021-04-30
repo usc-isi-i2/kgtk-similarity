be124c7ff4fd60a651967eb7b7a775b0d1600521
saggu <amandeep.s.saggu@gmail.com>
2021-04-30 16:39:52 -0700

    add paths endpoint

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from app_config import host, port
 from semantic_similarity.main import QnodeSimilarity
 from semantic_similarity.main import NN
+from semantic_similarity.main import Paths
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,7 @@ api = Api(app)
 
 api.add_resource(NN, '/nearest-neighbors')
 api.add_resource(QnodeSimilarity, '/similarity_api')
+api.add_resource(Paths, '/paths')
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
