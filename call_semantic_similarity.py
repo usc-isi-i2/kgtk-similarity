9b29c1908c0d5bc95517018b749e7c5ddcd39088
Hans Chalupsky <chalupsky@verizon.net>
2021-10-11 18:31:35 -0700

    Updated for v2 API

--- call_semantic_similarity.py
+++ call_semantic_similarity.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 import pandas as pd
 
 
@@ -9,13 +10,12 @@ def call_semantic_similarity(input_file, url):
     files = {
         'file': (file_name, open(input_file, mode='rb'), 'application/octet-stream')
     }
-    resp = requests.post(url, files=files)
-
-    s = resp.json()
-
+    resp = requests.post(url, files=files, params={'similarity_types': 'all'})
+    
+    s = json.loads(resp.json())
+    
     return pd.DataFrame(s)
 
-
-url = 'https://dsbox02.isi.edu:8888/qnode-similarity'
-df = call_semantic_similarity('./test_file.tsv', url)
+url = 'https://kgtk.isi.edu/similarity_api'
+df = call_semantic_similarity('test_file.tsv', url)
 df.to_csv('test_file_similarity.tsv', index=False, sep='\t')
