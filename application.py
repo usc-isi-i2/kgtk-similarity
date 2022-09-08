1bb7bd7b8386f5403a93040211e9c8c6676c2d18
saggu <amandeep.s.saggu@gmail.com>
2022-09-08 15:57:33 -0700

    remove path api

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ from flask_restful import Api
 from app_config import host, port
 from semantic_similarity.main import QnodeSimilarity
 from semantic_similarity.main import NN
-from semantic_similarity.main import Paths
+
+# from semantic_similarity.main import Paths
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +14,7 @@ api = Api(app)
 
 api.add_resource(NN, '/nearest-neighbors')
 api.add_resource(QnodeSimilarity, '/similarity_api')
-api.add_resource(Paths, '/paths')
+# api.add_resource(Paths, '/paths')
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
